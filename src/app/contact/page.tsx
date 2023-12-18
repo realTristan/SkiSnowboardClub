23d94a48e8b0b3de4886086a6ae864bffa2c9e15
Tristan <heytristaann@gmail.com>
2023-12-18 09:56:21 -0400

    updated contact email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ import SocialMedia from "@/components/logos/SocialMediaLogos";
 import ExternalSVG from "@/components/svgs/External";
 
 export default function ContactPage() {
-  const contactEmail: string = "heytristaann@gmail.com";
+  const contactEmail: string = "skiandboard@uoguelph.ca";
 
   return (
     <>
