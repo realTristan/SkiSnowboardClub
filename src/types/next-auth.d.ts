5d0052f55e6415546ec9cdc7271fcb9b93349c25
Tristan <heytristaann@gmail.com>
2023-12-22 15:10:42 -0400

    update user interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,15 +5,17 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+  interface User {
+    id?: string;
+    secret: string | null;
+    name: string | null;
+    email: string | null;
+    image: string | null;
+    purchasedEventIds: string[];
+    permissions: Permission[];
+  }
+
   interface Session {
-    user: {
-      id?: string;
-      secret: string | null;
-      name: string | null;
-      email: string | null;
-      image: string | null;
-      purchasedEventIds: string[];
-      permissions: Permission[];
-    };
+    user: User;
   }
 }
